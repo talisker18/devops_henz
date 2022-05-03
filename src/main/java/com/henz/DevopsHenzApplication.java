f4fa8834198923106bf192ab7fa136f0c3de424d
talisker18 <hjoel87@gmx.ch>
2022-05-03 18:04:59 +0200

    .

---
+++ src/main/java/com/henz/DevopsHenzApplication.java
@@ -0,0 +1,13 @@
+package com.henz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsHenzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsHenzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henz/DevopsHenzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsHenzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
